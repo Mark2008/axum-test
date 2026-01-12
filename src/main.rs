fa1a019ac25a25967f4ba88940fe9d817d98392e
malkeu <kangmark.2008@gmail.com>
2026-01-12 22:47:34 +0900

    implement hwcj page

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    Json, Router,
+    routing::{get},
+    // http::StatusCode,
+    // Json, 
+    Router,
 };
-use serde::{Serialize, Deserialize};
+// use serde::{Serialize, Deserialize};
+use tower_http::services::ServeDir;
 
 #[allow(unused_imports)]
 use tracing::{debug, error, info, trace, warn};
@@ -14,43 +16,22 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     // build our application with a route
+    // nest_service: used for static files
     let app = Router::new()
         .route("/", get(root))
-        .route("/users", post(create_user));
+        .nest_service(
+            "/hwcj",
+            ServeDir::new("hwcj_static")
+        );
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     
-    info!("Serving on http://localhost:3000...");
+    info!("Start serving...");
     axum::serve(listener, app).await.unwrap();
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "안경 사세요"
-}
-
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
-
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    (StatusCode::CREATED, Json(user))
 }
