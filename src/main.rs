f4af52eb9255687f18f560d09480e618b958f02b
malkeu <kangmark.2008@gmail.com>
2026-01-13 23:09:52 +0900

    add logging

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,18 @@ use axum::{
 };
 // use serde::{Serialize, Deserialize};
 use tower_http::services::ServeDir;
+use tower_http::trace::TraceLayer;
 
 #[allow(unused_imports)]
 use tracing::{debug, error, info, trace, warn};
+use tracing_subscriber::filter::EnvFilter;
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::new("debug"))   // for logging all
+        .init();
 
     // build our application with a route
     // nest_service: used for static files
@@ -26,7 +30,8 @@ async fn main() {
         .nest_service(
             "/hwcj",
             ServeDir::new("static/hwcj")
-        );
+        )
+        .layer(TraceLayer::new_for_http());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
